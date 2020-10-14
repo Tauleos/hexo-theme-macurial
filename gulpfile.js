533c8c08652ca724865323046c8dcd168fc05d1e
Tauleos <847301609@qq.com>
2020-10-14 17:13:36 +0800

    feat: update gulp,update themes

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,16 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-// 一次性编译 Sass 
-gulp.task('sass', function() {
+function gulpScss() {
     return gulp.src('./source/scss/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(autoprefixer())
         .pipe(gulp.dest('./source/css'));
-});
-
-// 实时编译
-gulp.task('default', ['sass'], function() {
-    gulp.watch('./source/scss/_partial/*.scss', ['sass']);
-    gulp.watch('./source/scss/*.scss', ['sass']);
-});
+}
+// 一次性编译 Sass 
+gulp.task('sass',gulpScss);
+// exports.sass = sass;
+exports.default=function() {
+    console.log('aaa default');
+    gulp.watch(['./source/scss/_partial/*.scss','./source/scss/*.scss'],gulpScss);
+};
